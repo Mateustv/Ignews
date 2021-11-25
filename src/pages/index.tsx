e53714392abd3f9b706141a060a501dcbfb5de68
Mateus T <mateustavarestv@hotmail.com>
2021-11-25 11:34:18 -0300

    Criando a pagina Home e seus componentes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | Ig.news</title>
+        <title>Home | Ig.news</title>
       </Head>
-      <h1>
-        Hello world
-      </h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero} >
+          <span><span>👏</span> Hey, welcome!</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world
+          </h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9,90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl Conding" />
+      </main>
     </>
   )
 }
