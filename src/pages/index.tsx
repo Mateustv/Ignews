3882e5415daa3c0e56008dac021ec08e433f0810
Mateus T <mateustavarestv@hotmail.com>
2021-11-24 22:48:04 -0300

    terminei de configurar o projeto com typescript e sass

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | Ig.news</title>
+      </Head>
+      <h1>
+        Hello world
+      </h1>
+    </>
+  )
+}
