a91a92014180e25f0766dde7b5f0e35c3c03277c
Mateus T <mateustavarestv@hotmail.com>
2021-12-29 21:49:29 -0300

    interliguei com banco de dados se o status da assinatura do usario

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { Readable } from "stream"
 import Stripe from "stripe"
 import { stripe } from "../../services/stripe"
+import { saveSubscription } from "./_lib/manegaSubscriptions"
 
 // RECEBENDO OS DADOS VIA STREAMER //
 async function buffer(readable: Readable) {
@@ -23,7 +24,9 @@ export const config = {
 }
 //EVENTOS QUE QUERO MONITORAR //
 const relevantEvents = new Set([
-  'checkout.session.completed'
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -44,10 +47,37 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const { type } = event
 
     if (relevantEvents.has(type)) {
-      console.log('evento recebido', event)
+
+      try {
+        switch (type) {
+
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+            )
+            break
+
+          case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString()
+            )
+
+            break
+          default:
+            throw new Error('Unhandled Event')
+        }
+      } catch (err) {
+        return res.json({ error: 'Webhook handler failed' })
+      }
     }
 
-    res.status(200).json({ ok: true })
+    res.json({ ok: true })
   } else {
 
     res.setHeader('Allow', 'POST')
